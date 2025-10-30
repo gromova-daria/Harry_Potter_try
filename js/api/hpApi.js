12ea83154928027a4088a48c6083cee11cd021cf
chuprovatata <145992557+chuprovatata@users.noreply.github.com>
2025-10-30 04:26:48 +0300

    Update hpApi.js

--- js/api/hpApi.js
+++ js/api/hpApi.js
@@ -1,5 +1,5 @@
 const HPapi = {
-    url: 'https://hp-api.onrender.com/api',
+    url: 'https://potterhead-api.vercel.app/api',
 
     async getAllCharct() {
         console.log('Пытаемся загрузить персонажей');
@@ -20,12 +20,12 @@ const HPapi = {
     async getCharacterByName(characterName) {
         console.log(`Ищем персонажа: ${characterName}`);
         try {
-            
+
             const allCharacters = await this.getAllCharct();
-            const character = allCharacters.find(char => 
+            const character = allCharacters.find(char =>
                 char.name.toLowerCase() === characterName.toLowerCase()
             );
-            
+
             if (character) {
                 console.log(`Нашли персонажа: ${character.name}`);
                 return character;
@@ -36,6 +36,52 @@ const HPapi = {
         } catch (error) {
             console.error('Ошибка при поиске персонажа:', error);
             return null;
+        }
+    },
+
+    async getBooks() {
+        console.log('Пытаемся загрузить книги');
+        try {
+            const response = await fetch(`${this.url}/books`);
+            if (!response.ok) {
+                throw new Error('Не удалось загрузить данные');
+            }
+            const booksData = await response.json();
+            console.log(`Получили ${booksData.length} книг`);
+
+            const bookCovers = await mediaAPI.getBookCovers();
+
+            return booksData.map((book, index) => ({
+                ...book,
+                cover: bookCovers[index]?.cover || mediaAPI.getDefaultBookCover(),
+                id: book.serial || index.toString()
+            }));
+        } catch (error) {
+            console.error('Ошибка:', error);
+            return [];
+        }
+    },
+
+    async getMovies() {
+        console.log('Пытаемся загрузить фильмы');
+        try {
+            const response = await fetch(`${this.url}/movies`);
+            if (!response.ok) {
+                throw new Error('Не удалось загрузить данные');
+            }
+            const moviesData = await response.json();
+            console.log(`Получили ${moviesData.length} фильмов`);
+
+            const moviePosters = await mediaAPI.getMoviePosters();
+
+            return moviesData.map((movie, index) => ({
+                ...movie,
+                poster: moviePosters[index]?.Poster || mediaAPI.getDefaultMoviePoster(),
+                id: movie.serial || index.toString()
+            }));
+        } catch (error) {
+            console.error('Ошибка:', error);
+            return [];
         }
     }
 };
